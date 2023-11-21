daa1c83323149c6a1edab33581540cefdd4a2097
haziqshahard <hhs21@ic.ac.uk>
2023-11-21 16:00:37 +0000

    Added printing integers 1 and 10 inclusive

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,3 @@
-package org.example;
-
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
@@ -7,5 +5,8 @@ public class Main {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello world!");
+        for(int i=1;i<11;i++){
+            System.out.println(i);
+        }
         }
     }
